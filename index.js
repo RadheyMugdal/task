f14e6ad6eb85bdf0459581cb26b33eb07c7757ee
Radhey Mugdal <mugdalradhey@gmail.com>
2024-04-15 14:30:26 +0530

    cors error

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-const express=require('express')
-const data=require('./data.js')
-const cors=require('cors')
+const express = require('express');
+const data = require('./data.js');
+const cors = require('cors');
 
-express()
-    .use(cors(
-        {
-            origin:'*',
-            methods:'GET,POST,PUT,DELETE,OPTIONS',
-            allowedHeaders:['Content-Type','Authorization','X-Requested-With']
-        }
-    ))
-    .use(express.json())
-    .get('/',(req,res)=>{
-        res.send(data)
-    })
-    .listen(3000,()=>{
-        console.log('server is running on port 3000')
-    })
+const app = express();
 
-const app=express()
+app.use(cors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 
-app.get('/',(req,res)=>{
-    res.send(data)
-})
+app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000')
-})
+app.get('/', (req, res) => {
+    res.send(data);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
