761a5e0543705f36d718c28075b2fd14affd78d3
Radhey Mugdal <mugdalradhey@gmail.com>
2024-04-15 14:26:19 +0530

    cors error

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express=require('express')
 const data=require('./data.js')
+const cors=require('cors')
 
+express()
+    .use(cors())
+    .use(express.json())
+    .get('/',(req,res)=>{
+        res.send(data)
+    })
+    .listen(3000,()=>{
+        console.log('server is running on port 3000')
+    })
 
 const app=express()
 
