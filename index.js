e6de313b5d1a98047cdfaf9d4f5f1b23918a1578
Radhey Mugdal <mugdalradhey@gmail.com>
2024-04-15 15:32:31 +0530

    cors removed

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const data = require('./data.js');
-const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-}));
 
 app.use(express.json());
 
